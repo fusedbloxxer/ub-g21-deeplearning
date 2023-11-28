bb71256db285abf8df65a0f8c6bf137a3a43adb6
fusedbloxxer <voidwarden@yahoo.com>
2023-11-28 02:44:02 +0200

    Add MLFlow

--- projects/gic/src/data.py
+++ projects/gic/src/data.py
@@ -8,30 +8,28 @@ import pandas as ps
 import torch.utils.data as data
 
 
-DataSplit: t.TypeAlias = t.Literal['train', 'val', 'test']
-DatasetXY: t.TypeAlias = data.Dataset[t.Union[t.Tuple[Tensor, Tensor], Tensor]]
-
-
-class GenImageDataset(DatasetXY):
-    def __init__(self, path: pl.Path, split: DataSplit, preprocess: bool = True) -> None:
+class GenImageDataset(data.Dataset[t.Tuple[Tensor, Tensor] | Tensor]):
+    def __init__(self,
+                 path: pl.Path,
+                 split: t.Literal['train', 'val', 'test'],
+                 preprocess: bool = True) -> None:
         super(GenImageDataset, self).__init__()
-        self.__split = split
 
+        self.__split = split
         self.__root = path
         self.__meta = self.__root / f'{self.__split}.csv'
         self.__imag = self.__root / f'{self.__split}_images'
 
-        self.data_ = ps.read_csv(self.__meta)
-
+        self.__data = ps.read_csv(self.__meta)
         self.__preprocess = preprocess
         self.__transform = Compose([
             ToDtype(dtype=torch.float32, scale=True),
-            Resize(size=(224, 244), antialias=True),
+            # Resize(size=(224, 244), antialias=True),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
-    def __getitem__(self, index: int) -> t.Tuple[Tensor, Tensor] | Tensor:
-        image_name: str = self.data_.iloc[index].loc['Image']
+    def __getitem__(self, index: int):
+        image_name: str = self.__data.iloc[index].loc['Image']
         image_path: str = str(self.__imag / image_name)
         image: Tensor = tn.io.read_image(image_path, tn.io.ImageReadMode.RGB)
 
@@ -41,20 +39,16 @@ class GenImageDataset(DatasetXY):
         if self.__split == 'test':
             return image
 
-        label: Tensor = torch.tensor(self.data_.iloc[index].loc['Class'])
+        label: Tensor = torch.tensor(self.__data.iloc[index].loc['Class'])
         return image, label
 
     def __len__(self) -> int:
-        return len(self.data_)
+        return len(self.__data)
 
 
-class AugementedDataset(DatasetXY):
-    def __init__(self, dataset: DatasetXY) -> None:
-        super().__init__()
-        self.dataset_ = dataset
-
-    def __getitem__(self, index: int) -> t.Tuple[Tensor, Tensor] | Tensor:
-        return self.dataset_.__getitem__(index)
-
-    def __len__(self) -> int:
-        return len(t.cast(t.Sequence, self.dataset_))
+def load_data(path: pl.Path) -> t.Tuple[data.Dataset[t.Tuple[Tensor, Tensor]], data.Dataset[Tensor]]:
+    test_data = GenImageDataset(path, 'test')
+    valid_data = GenImageDataset(path, 'val')
+    train_data = GenImageDataset(path, 'train')
+    train_data = data.ConcatDataset([train_data, valid_data])
+    return train_data, t.cast(data.Dataset[Tensor], test_data)
--- projects/gic/src/model.py
+++ projects/gic/src/model.py
@@ -8,7 +8,7 @@ class ResConvBlock(nn.Module):
     def __init__(self, in_chan: int, out_chan: int) -> None:
         super(ResConvBlock, self).__init__()
 
-        self.drop = nn.Dropout2d(p=0.2)
+        self.drop = nn.Dropout2d(p=0.4)
         self.aux_layer = nn.Conv2d(in_chan, out_chan, kernel_size=1, stride=1, padding=0)
         self.conv_layer1 = nn.Conv2d(in_chan, out_chan, kernel_size=3, stride=1, padding=1)
         self.conv_layer2 = nn.Conv2d(out_chan, out_chan, kernel_size=3, stride=1, padding=1)
@@ -32,17 +32,20 @@ class ResCNN(nn.Module):
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.SiLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            ResConvBlock(in_chan=64, out_chan=128),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            ResConvBlock(in_chan=128, out_chan=256),
+            ResConvBlock(in_chan=64, out_chan=256),
             nn.MaxPool2d(kernel_size=2, stride=2),
             ResConvBlock(in_chan=256, out_chan=512),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            ResConvBlock(in_chan=512, out_chan=1024),
             nn.AdaptiveAvgPool2d(output_size=1),
             nn.Flatten(),
+            nn.Linear(in_features=1024, out_features=512),
+            nn.SiLU(),
+            nn.BatchNorm1d(512),
+            nn.Dropout1d(p=0.3),
             nn.Linear(in_features=512, out_features=512),
             nn.SiLU(),
-            nn.Linear(in_features=512, out_features=512),
-            nn.SiLU(),
+            nn.BatchNorm1d(512),
             nn.Linear(in_features=512, out_features=100),
         )
 
